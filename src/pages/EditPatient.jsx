43932df07424ab9efb482efe0139afc29463fa81
tomasfesci <tomasfesci@gmail.com>
2026-03-31 22:53:02 -0300

    Se agrego la etiqueta de sobrepeso

--- src/pages/EditPatient.jsx
+++ src/pages/EditPatient.jsx
@@ -7,7 +7,7 @@ export default function EditPatient() {
   const navigate = useNavigate();
   const availableTags = [
     'Vegano', 'Vegetariano', 'Celiaquía', 'Sedentario', 'Deportista',
-    'Adulto Mayor', 'Obesidad', 'Diabetes', 'TCA', 'Embarazo/Lactancia'
+    'Adulto Mayor', 'Sobrepeso', 'Obesidad', 'Diabetes', 'TCA', 'Embarazo/Lactancia'
   ];
 
   const [formData, setFormData] = useState({
